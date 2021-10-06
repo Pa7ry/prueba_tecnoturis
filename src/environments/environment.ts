c2c77bc27e791fc265aad9c5f0691a8d539bd825
pa7ry <patriciadesantiagogallego@gmail.com>
2021-10-06 02:54:16 +0200

    feat: firebase added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,19 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDenxQBZenfGmbyHGibAzARF8ySnPo7tvI",
+  authDomain: "prueba-tecnoturis.firebaseapp.com",
+  projectId: "prueba-tecnoturis",
+  storageBucket: "prueba-tecnoturis.appspot.com",
+  messagingSenderId: "296825118512",
+  appId: "1:296825118512:web:271b88a4ca6996e2b032f5",
+  measurementId: "G-P363H0ZDG9",
+};
+
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig,
 };
 
 /*
