8658cf58dc09cc2d3e73d2af63192e531b29f001
pa7ry <patriciadesantiagogallego@gmail.com>
2021-10-06 10:48:38 +0200

    feat: auth service

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  auth = getAuth();
+
+  constructor() { }
+
+  createUser(email: string, password: string) {
+    createUserWithEmailAndPassword(this.auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+  }
+
+  singIn(email: string, password: string) {
+    signInWithEmailAndPassword(this.auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  }
+}
